844cad5f5f1e907e8c70393050c9dd22ca121fad
laynagar <laynagar940@gmail.com>
2020-07-26 16:13:40 +0530

    replaced index page's images with compressed static images

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings 
 from django.conf.urls.static import static 
-from .views import IndexView, PortfolioView1, PortfolioView2, PortfolioView3, Contact, About
+from .views import IndexView, PortfolioView1, PortfolioView2, Contact, About
 
 
 app_name = 'Gallery'
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('portfolio1/', PortfolioView1.as_view(), name='portfolio-1'),
     path('portfolio2/', PortfolioView2.as_view(), name='portfolio-2'),
-    path('portfolio3/', PortfolioView3.as_view(), name='portfolio-3'),
     path('contact/', Contact.as_view(), name='contact'),
     path('about/', About.as_view(), name='about'),
 ]
--- Gallery/views.py
+++ Gallery/views.py
@@ -6,15 +6,18 @@ from django.urls import reverse_lazy
 
 
 
-class IndexView(ListView):
-    template_name = 'Gallery/index.html' 
-    model = PictureModel
-    paginate_by = 30
-
+class IndexView(TemplateView):
+    template_name = 'Gallery/index.html'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+        context['r1'] = range(1,8)
+        context['r2'] = range(8,15)
+        context['r3'] = range(14,17)
+
         return context
+    
+
 
 class PortfolioView1(ListView):
     model = PictureModel
@@ -38,15 +41,6 @@ class PortfolioView2(ListView):
         context['categories'] = PictureCategoryModel.objects.distinct()
         return context
 
-class PortfolioView3(ListView):
-    model = PictureModel
-    # paginate_by = 10  
-    template_name = 'Gallery/portfolio-2.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = PictureCategoryModel.objects.distinct()
-        return context
 
 class Contact(FormView):
     template_name = 'Gallery/contact.html'
